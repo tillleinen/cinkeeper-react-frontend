899d36a2ec4c6f1863aae3de9b4ba290c811b930
Till Leinen <hello@till.is>
2015-04-02 11:15:08 +0200

    indention

--- src/scripts/components/Film.js
+++ src/scripts/components/Film.js
@@ -7,13 +7,13 @@ var { Route, DefaultRoute, RouteHandler, Link } = Router;
 require('styles/Film.sass');
 
 var Film = React.createClass({
-  render: function () {
-    return (
-      <div className="film">
-        <RouteHandler />
-      </div>
-    );
-  }
+    render: function () {
+        return (
+            <div className="film">
+                <RouteHandler />
+            </div>
+        );
+    }
 });
 
 module.exports = Film; 
