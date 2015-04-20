1c284ffe29435fae14d5bb56c0e3c8240fc6e0a0
Till Leinen <hello@till.is>
2015-04-20 22:55:31 +0200

    clean up

--- src/scripts/components/PhotoRow.js
+++ src/scripts/components/PhotoRow.js
@@ -10,22 +10,15 @@ var scrollInterval;
 
 var PhotoRow = React.createClass({
   componentDidMount: function () {
-    // $(document).on('scroll', this.handleScroll);
-    scrollInterval = setInterval(this.handleScroll, 10);
+    scrollInterval = setInterval(this.handleInterval, 10);
   },
 
   componentWillUnmount: function () {
-    // $(document).off('scroll', this.handleScroll);  
     clearInterval(scrollInterval);
   },
 
-  handleScroll: function () {
+  handleInterval: function () {
     window.requestAnimationFrame(this.moveY);
-
-    // console.log(scrollDiff);
-    // console.log(scrollPercentage);
-    // console.log(sectionHeight);
-    // console.log(rowHeight);
   },
 
   moveY: function () {
@@ -39,10 +32,6 @@ var PhotoRow = React.createClass({
     var scrollPercentage = $(window).scrollTop() / scrollDiff;
 
     var necessaryOffset = (diff * scrollPercentage).toFixed(2);
-
-    console.log(rowNode);
-    console.log(necessaryOffset);
-    console.log("======");
 
     rowNode.css('transform', 'translate3d(0,' + necessaryOffset + 'px,0)');    
   },
