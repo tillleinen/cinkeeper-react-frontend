4bce3277a27442b59e7b2e13752884398a177b26
Till Leinen <hello@till.is>
2015-04-21 16:58:05 +0200

    render videos from json

--- src/scripts/components/Videos.js
+++ src/scripts/components/Videos.js
@@ -26,13 +26,11 @@ var Videos = React.createClass({
                     <VideoCategoryItem selectedCategory={category.slug} data={category} onClick={this.transitionToCategories} isClosable={true} />
                 </ul>
                 <ul className="video-list">
-                    <VideoItem vimeoId="113510208" imageSrc="../../images/video5.jpg" caption="JD Collection 2014 x THREE" />
-                    <VideoItem vimeoId="107468450" imageSrc="../../images/video1.jpg" caption="Mortis x Making Of x Hollywoodpsychose x 2014" />
-                    <VideoItem vimeoId="116354206" imageSrc="../../images/video3.jpg" caption="Klangpoet x 4U x Wildcard Contest ESC 2015" />
-                    <VideoItem vimeoId="114948026" imageSrc="../../images/video4.jpg" caption="Fuchsteufelswild x PARADIGMA x Teaser I" />
-                    <VideoItem vimeoId="110206885" imageSrc="../../images/video6.jpg" caption="FAVRY x PRAY x 2014" />
-                    <VideoItem vimeoId="113278713" imageSrc="../../images/video7.jpg" caption="THE WORLD IS YOURS x 99FFA x 2015 x TOP99" />
-                    <VideoItem vimeoId="121350228" imageSrc="../../images/video2.jpg" caption="CINEKEEPER x SHOWREEL x 2015" />
+                    {
+                        category.videos.map(function (video) {
+                            return <VideoItem vimeoId={video.id} imageSrc={video.image.image.url} caption={video.name} />;
+                        })
+                    }
                 </ul>
             </div>
         );
