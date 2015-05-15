d86dd084be8c9a57dc7d022b9e988a95bababf5b
Till Leinen <hello@till.is>
2015-05-15 14:34:50 +0200

    clients page approach

--- src/scripts/components/Clients.js
+++ src/scripts/components/Clients.js
@@ -7,8 +7,19 @@ require('styles/Clients.sass');
 var Clients = React.createClass({
   render: function () {
     return (
-        <div>
-          <p>Content for Clients</p>
+        <div className="clients">
+        	<div className="clients__bg-image"></div>
+        	<div className="clients-overlay">
+	        	<h1 className="clients-overlay__headline">Clients</h1>
+	        	<ul className="clients-list">
+	        		<li className="clients-list__item" style={{'background-image': 'url(../../images/jwlry.png)'}}></li>
+	        		<li className="clients-list__item" style={{'background-image': 'url(../../images/berolina.png)'}}></li>
+	        		<li className="clients-list__item" style={{'background-image': 'url(../../images/emp.png)'}}></li>
+	        		<li className="clients-list__item" style={{'background-image': 'url(../../images/jussi.png)'}}></li>
+	        		<li className="clients-list__item" style={{'background-image': 'url(../../images/favry.png)'}}></li>
+	        		<li className="clients-list__item" style={{'background-image': 'url(../../images/fuchsteufelswild-laurel-leaves-logo.png)'}}></li>
+	        	</ul>
+        	</div>
         </div>
       );
   }
