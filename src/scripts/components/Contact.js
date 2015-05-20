2269a116969313bacf32e84538287e4155abf303
Till Leinen <hello@till.is>
2015-05-20 14:20:03 +0200

    add contact page

--- src/scripts/components/Contact.js
+++ src/scripts/components/Contact.js
@@ -7,9 +7,26 @@ require('styles/Contact.sass');
 var Contact = React.createClass({
   render: function () {
     return (
-        <div>
-          <p>Content for Contact</p>
-        </div>
+		<div className="contact-container">
+        	<div className="contact__bg-image"></div>
+        	<div className="contact__content">
+        		<h1 className="contact__content__headline">Contact</h1>
+        		<p>
+        			CINEKEEPER<br/>
+        			Wambeler Hellweg 40<br/>
+					44143 Dortmund <br/>
+					Germany <br/>
+        		</p>
+        		<p>
+        			Telefon: +49 (0) 176 821 89544<br/>
+					Telefon: +49 (0) 176 327 64290
+        		</p>
+        		<p>
+        			<a href="mailto:info@cinekeeper.de">info@cinekeeper.de</a><br/>
+					<a href="http://www.cinekeeper.de">www.cinekeeper.de</a>
+        		</p>
+        	</div>	
+		</div>
       );
   }
 });
