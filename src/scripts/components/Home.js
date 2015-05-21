eca000575d46828983f21ea6afff1d1ff1b91296
Till Leinen <hello@till.is>
2015-05-21 16:14:51 +0200

    fetch home image from API

--- src/scripts/components/Home.js
+++ src/scripts/components/Home.js
@@ -1,13 +1,50 @@
 'use strict';
 
 var React = require('react/addons');
+var Request = require('../utils/Request.js');
 
 require('styles/Home.sass');
 
 var Home = React.createClass({
+	getInitialState: function () {
+		return {
+			background_images: []
+		};
+	},
+
+	componentDidMount: function() {
+		this.fetchData();
+	},
+
+	fetchData: function () {
+		Request
+			.get('/background_images')
+			.end(this.handleResponse);
+	},
+
+	handleResponse: function (err, res) {
+		if(err) {
+			console.log(err);
+		} else if(this.isMounted()) {
+			var background_images = JSON.parse(res.text).background_images;
+			this.setState({
+				background_images: background_images
+			});
+		}
+	},
+
+	composeStyle: function () {
+		if (this.state.background_images.length === 0) {
+			return {};
+		}
+		return {
+			'background-image': 'url(' + this.state.background_images[0].image.image.medium.url + ')'
+		};
+	},
+
   render: function () {
     return (
-	  <div className="home">
+	  <div className="home" style={this.composeStyle()}>
 	  </div>
 	);
   }
