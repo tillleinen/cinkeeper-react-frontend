0a5c73ba4eb17e62013bb4dd64ffc88b134690c2
Till Leinen <hello@till.is>
2015-09-02 17:32:06 +0200

    Add new background for about

--- src/scripts/components/AboutItem.js
+++ src/scripts/components/AboutItem.js
@@ -18,7 +18,7 @@ var AboutItem = React.createClass({
   render: function () {
     var content = 
       <div>
-        <div className="about-overview__item__image" style={{'background-image': 'url(' + this.props.imageSrc + ')'}}></div>
+        <div className="about-overview__item__image"></div>
         <div className="about-overview__item__overlay">
             <div className="about-overview__item__overlay__caption">
                 {this.props.linkTo ? this.props.text: ''}
